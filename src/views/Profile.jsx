1591636ac41a99a114a1ac519d0cd72f8258837b
iStrafe <Yugotabata@gmail.com>
2025-03-14 15:43:44 +0800

    Additional module for perofile setting

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -43,21 +43,35 @@ const Profile = () => {
   return (
     <div className="grid grid-cols-4 h-screen bg-slate-900">
       <div className="col-span-4 place-self-center">
-        <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl gap p-5">        
-            <span className="text-pink-600">Profile</span> <span className="text-cyan-300">Settings</span>
-        </h1>        
-        {userData ? (
-          <div className="bg-gray-800 text-white p-4 rounded-3xl shadow-md max-w-md">
-            <h3 className="text-lg font-semibold">{userData.firstName} {userData.middleName} {userData.lastName}</h3>
-            <p>Email: {userData.email}</p>
-            <div className="bg-gray-900">
-               {userData.image && <img src={`https://localhost:7056/${userData.image}`} alt="Profile" onError={(e) => e.target.src = "/default-profile.png"}/>}
-            </div>
-            <p className="mt-2">Description: {userData.description}</p>
+          <div className="grid grid-cols-5 gap-5 py-10">
+          
+                <div className="col-span-4">
+                  <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl gap p-5">        
+                  <span className="text-pink-600">Profile</span> <span className="text-cyan-300">Settings</span>
+                  </h1>      
+                </div>  
+
+                {userData ? (
+                  <div className="col-span-2 bg-gray-800 text-white p-4 rounded-3xl shadow-md max-w-md">
+                    <h3 className="text-lg font-semibold">{userData.firstName} {userData.middleName} {userData.lastName}</h3>
+                    <p>Email: {userData.email}</p>
+                    <div className="bg-gray-900">
+                      {userData.image && <img src={`https://localhost:7056/${userData.image}`} alt="Profile" onError={(e) => e.target.src = "/default-profile.png"}/>}
+                    </div>
+                    <p className="mt-2">Description: {userData.description}</p>
+                  </div>
+                ) : (
+                  <p>Loading profile...</p>
+                )}
+
+                <div className="col-span-3 bg-gray-800 text-white p-4 rounded-3xl shadow-md">
+                  <h1 className="font-bold text-3xl gap p-5">
+                  <span className="text-gray-400">EDIT</span> <span className="text-gray-300">PROFILE</span></h1>
+                  <div className="grid grid-cols-3">
+                    
+                  </div>
+                </div>
           </div>
-        ) : (
-          <p>Loading profile...</p>
-        )}
       </div> 
 
     </div>
